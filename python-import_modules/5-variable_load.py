52e036e569a1a9af4fc5ed83be97a01179168c76
jbn179 <9772@holbertonstudents.com>
2025-01-15 07:32:51 +0100

    python-import_modules

---
+++ python-import_modules/5-variable_load.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+from variable_load_5 import a
+
+
+def main():
+    """
+    Print the value of the variable 'a' imported from variable_load_5.py.
+    """
+    print(a)
+
+
+if __name__ == "__main__":
+    main()
