26396e9074e6843d6fc042580b32f579524358a7
jbn179 <9772@holbertonstudents.com>
2025-01-23 11:32:05 +0100

    python-exceptions

---
+++ python-exceptions/0-safe_print_list.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+    """
+    Print the first x elements of a list safely.
+
+    Parameters:
+    my_list (list): The list to print elements from.
+    x (int): The number of elements to print.
+
+    Returns:
+    int: The actual number of elements printed.
+    """
+    try:
+        for i in range(x):
+            print(my_list[i], end="")
+        print()
+        return x
+    except IndexError:
+        print()
+        return i
+    
