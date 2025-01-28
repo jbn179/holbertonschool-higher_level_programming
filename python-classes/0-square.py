a4831f7e228a609c112eb381055acb7300aa0f3c
jbn179 <9772@holbertonstudents.com>
2025-01-28 12:30:26 +0100

    python-classes

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -4,6 +4,7 @@
 This module defines an empty class Square that defines a square.
 """
 
+
 class Square:
     """An empty class that defines a square."""
     pass
--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-
 """
 This module defines a class Square that defines a square by its size.
 """
 
+
 class Square:
     """A class that defines a square by its size."""
-    
+
     def __init__(self, size):
         """Initialize the square with a private instance attribute size."""
         self.__size = size
