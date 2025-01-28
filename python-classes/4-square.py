cfbd2a6738e3e581705850dedc4c9450e1164162
jbn179 <9772@holbertonstudents.com>
2025-01-28 14:02:29 +0100

    python-classes

---
+++ python-classes/4-square.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""This module defines a class Square that defines a square by its size."""
+
+
+class Square:
+    """A class that defines a square by its size."""
+
+    def __init__(self, size=0):
+        """Initialize the square with a private instance attribute size."""
+        self.size = size
+
+    @property
+    def size(self):
+        """Retrieve the size of the square."""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Set the size of the square with type and value validation."""
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Return the area of the square."""
+        return self.__size ** 2
