6b386204033f7c39d0d419a65873478618e6305d
jbn179 <9772@holbertonstudents.com>
2025-01-30 14:15:57 +0100

    python-more-class task 6

--- python-more_classes/6-rectangle.py
+++ python-more_classes/6-rectangle.py
@@ -22,6 +22,7 @@ class Rectangle:
         """
         self.width = width
         self.height = height
+        Rectangle.number_of_instances += 1
 
     @property
     def width(self):
@@ -93,3 +94,4 @@ class Rectangle:
     def __del__(self):
         """Print a message for every deletion of a rectangle."""
         print("Bye rectangle...")
+        Rectangle.number_of_instances -= 1
