108f76b6a58a4e105ab73a443d3c70f3e00ef435
jbn179 <9772@holbertonstudents.com>
2025-02-01 14:02:10 +0100

    add task 2

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+""" Module that checks if an object is an instance of a class """
+
+
+def is_same_class(obj, a_class):
+    """ Function that checks if an object is an instance of a class """
+    return type(obj) == a_class
