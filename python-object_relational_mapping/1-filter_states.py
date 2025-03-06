1c5fc3ce072ba0205f158a74ebfe6f0584b65f09
jbn179 <9772@holbertonstudents.com>
2025-03-06 13:32:18 +0100

    add task 1

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""Script that lists all states with names starting with N"""
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    # Check if all 3 arguments are provided
+    if len(sys.argv) != 4:
+        print("Usage: {} username password database".format(sys.argv[0]))
+        sys.exit(1)
+
+    # Get command line arguments
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    # Connect to MySQL server
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=database
+    )
+
+    # Create cursor to execute queries
+    cursor = db.cursor()
+
+    # Execute SELECT query with LIKE filter
+    cursor.execute("""
+        SELECT * FROM states 
+        WHERE name LIKE 'N%' 
+        ORDER BY states.id
+    """)
+
+    # Fetch and print results
+    for row in cursor.fetchall():
+        print(row)
+
+    # Close cursor and database connection
+    cursor.close()
+    db.close()
