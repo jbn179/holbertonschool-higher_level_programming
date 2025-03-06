14a0d957c93a5c965e032ad5f6492bf8cd80519f
jbn179 <9772@holbertonstudents.com>
2025-03-06 11:53:33 +0100

    add task 0 and README

---
+++ python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""Script that lists all states from the database hbtn_0e_0_usa"""
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    # Check if all 3 arguments are provided
+    if len(sys.argv) != 4:
+        print("Usage: {} username password database".format(sys.argv[0]))
+        sys.exit(1)
+
+    # Get command line arguments
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    # Connect to MySQL server
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=database
+    )
+
+    # Create cursor to execute queries
+    cursor = db.cursor()
+
+    # Execute SELECT query
+    cursor.execute("SELECT * FROM states ORDER BY states.id")
+
+    # Fetch all rows
+    rows = cursor.fetchall()
+
+    # Print results
+    for row in rows:
+        print(row)
+
+    # Close cursor and database connection
+    cursor.close()
+    db.close()
