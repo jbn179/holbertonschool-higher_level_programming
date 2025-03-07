218598490958a5d5483b07577770557cd5bc4fa5
jbn179 <9772@holbertonstudents.com>
2025-03-07 20:23:50 +0100

    fix task 13

--- python-object_relational_mapping/13-model_state_delete_a.py
+++ python-object_relational_mapping/13-model_state_delete_a.py
@@ -27,8 +27,10 @@ if __name__ == "__main__":
     session = Session()
 
     # Query and delete states containing 'a'
-    states_to_delete = session.query(State).filter(State.name.like('%a%')).all()
-    for state in states_to_delete:
+    states = session.query(State).filter(
+        State.name.like('%a%')
+    ).all()
+    for state in states:
         session.delete(state)
     session.commit()
 
